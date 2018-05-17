1414d3e1e68db5a64b07bfa3b8a27c4938e0789d
Marian <marcinsekula3@gmail.com>
2018-05-17 21:53:54 +0200

    updated

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,6 +1,259 @@
+import random
 import tkinter as tk
-from MineField import *
-from ButtonField import *
+from tkinter import messagebox
+
+
+class ButtonField(tk.Button):
+    empty = None
+    flag = None
+    qmark = None
+    cheat = None
+    bomb = None
+    pbomb = None
+    tiles = []
+
+    def __init__(self,  *args, **kwargs):
+        tk.Button.__init__(self, command= self.leftclick, *args, **kwargs)
+        self.config(image=self.empty)
+        self.__Mine = False
+        self.__checked = False
+        self.bind('<Button-3>', self.rightclick)
+        self.clicks = 0
+        self.__flagged = False
+
+    def setCallback(self, function):
+        self.callback = function
+
+
+    def leftclick(self, *args):
+        for arg in args:
+            if arg == True:
+                if self.hasMine():
+                    return
+        if self.isChecked() or self.clicks == 1:
+            pass
+        else:
+            self.__checked = True
+            if self.hasMine():
+                self.config(image = self.bomb)
+                self.callback(1)
+            else:
+                mines = self.minesAround()
+                self.config(image = self.tiles[mines])
+                if self.minesAround() == 0:
+                    self.callback(-1, self)
+                else:
+                    self.callback(0)
+
+    def rightclick(self, event):
+        if self.__checked:
+            return
+        self.clicks = (self.clicks + 1) % 3
+        if self.clicks == 1:
+            self.config(image=self.flag)
+            self.__flagged = True
+            self.callback(2)
+        elif self.clicks == 2:
+            self.config(image=self.qmark)
+            self.callback(3)
+        else:
+            self.config(image=self.empty)
+            self.__flagged = False
+            self.callback(4)
+
+
+    def endGame(self):
+        if not self.isChecked():
+            self.__checked = True
+            if self.hasMine():
+                if self.clicks != 1: #Bomb is here <flag mark>
+                    self.config(image=self.pbomb)
+            else:
+                self.config(image=self.tiles[self.__minesAmount])
+
+
+    def highlight(self):
+        if self.hasMine():
+            self.config(image = self.cheat)
+
+    def isFlagged(self):
+        return self.__flagged
+
+    def isChecked(self):
+        return self.__checked
+
+    def setMine(self):
+        self.__Mine = True
+
+    def hasMine(self):
+        return self.__Mine
+
+    def setMineAmount(self, amount):
+        self.__minesAmount = amount
+
+    def minesAround(self):
+        return self.__minesAmount
+
+
+class MineField(object):
+    def __init__(self, dimX, dimY, mineNumber, callback):
+        board = []
+        ButtonField.empty = tk.PhotoImage(file="icons/empty.gif")
+        ButtonField.flag = tk.PhotoImage(file="icons/flag.gif")
+        ButtonField.qmark = tk.PhotoImage(file="icons/qmark.gif")
+        ButtonField.bomb = tk.PhotoImage(file="icons/bomb.gif")
+        ButtonField.pbomb = tk.PhotoImage(file="icons/pbomb.gif")
+        ButtonField.cheat = tk.PhotoImage(file="icons/cheat.gif")
+
+        for i in range(0, 9):
+            path = "icons/n" + str(i) + ".gif"
+            ButtonField.tiles.append(tk.PhotoImage(file=path))
+
+        for i in range(0, dimX, 1):
+            board.append([])
+            for j in range(0, dimY, 1):
+                board[i].append(ButtonField())
+                board[i][j].setCallback(callback)
+        self.__board = board
+        self.setMines(mineNumber)
+        self.checkIfNeighboursHasMines()
+
+
+    def getBoard(self):
+        return self.__board
+
+
+    def setMines(self, mineNumber):
+            random.seed()
+            mineCreated = 0
+            while mineCreated < mineNumber:
+                xrand = random.randrange(0,len(self.__board),1)
+                yrand = random.randrange(0,len(self.__board[0]),1)
+                if(self.__board[xrand][yrand].hasMine()):
+                    pass
+                else:
+                    self.__board[xrand][yrand].setMine()
+                    mineCreated+=1
+
+    def hasMine(self, x, y):
+        return self.__board[x][y].hasMine()
+
+    def minesAround(self, x, y):
+        return self.__board[x][y].minesAround()
+
+    def checkIfNeighboursHasMines(self):
+        for i in range(0, len(self.__board), 1):
+            for j in range(0, len(self.__board[0]), 1):
+                minesAmount = 0
+                # for x in range(0,3):
+                #     for y in range (0, 3):
+                #         try:
+                #             if x == 1:
+                #                 if y == 1:
+                #                     raise IndexError("its this field")
+                #             if self.__board[i+1-y][j+1-x].hasMine(): # [j+1-x] = j+1, j, j-1
+                #                 minesAmount+=1 # [i+1-y] = i+1, i, i-1
+                #         except IndexError as e:
+                #             print(i," ", j, " ", e)
+                #             pass
+                # self.__board[i][j].setMineAmount(minesAmount)
+                #all this below can be replaced with this, but sth wrong with 1st column
+
+                if i == 0:
+                    if j == 0:
+                        if(self.hasMine(i, j+1)):
+                            minesAmount+=1
+                        if(self.hasMine(i+1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i+1, j+1)):
+                            minesAmount+=1
+                    elif j == len(self.__board[i]) - 1:
+                        if(self.hasMine(i, j-1)):
+                            minesAmount+=1
+                        if(self.hasMine(i+1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i+1, j-1)):
+                            minesAmount+=1
+                    else:
+                        if(self.hasMine(i, j-1)):
+                            minesAmount+=1
+                        if(self.hasMine(i, j+1)):
+                            minesAmount+=1
+                        if(self.hasMine(i+1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i+1, j+1)):
+                            minesAmount+=1
+                        if(self.hasMine(i+1, j-1)):
+                            minesAmount+=1
+
+                elif i == len(self.__board) - 1:
+                    if j == 0:
+                        if(self.hasMine(i, j+1)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j+1)):
+                            minesAmount+=1
+                    elif j == len(self.__board[i]) - 1:
+                        if(self.hasMine(i, j-1)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j-1)):
+                            minesAmount+=1
+                    else:
+                        if(self.hasMine(i, j-1)):
+                            minesAmount+=1
+                        if(self.hasMine(i, j+1)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j+1)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j-1)):
+                            minesAmount+=1
+                elif j == 0:
+                        if(self.hasMine(i+1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i, j+1)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i+1, j+1)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j+1)):
+                            minesAmount+=1
+                elif j == len(self.__board[i]) - 1:
+                        if(self.hasMine(i+1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i, j-1)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j)):
+                            minesAmount+=1
+                        if(self.hasMine(i-1, j-1)):
+                            minesAmount+=1
+                        if(self.hasMine(i+1, j-1)):
+                            minesAmount+=1
+                else:
+                    if(self.hasMine(i+1, j)):
+                        minesAmount+=1
+                    if(self.hasMine(i-1, j)):
+                        minesAmount+=1
+                    if(self.hasMine(i, j+1)):
+                        minesAmount+=1
+                    if(self.hasMine(i, j-1)):
+                        minesAmount+=1
+                    if(self.hasMine(i+1, j-1)):
+                        minesAmount+=1
+                    if(self.hasMine(i-1, j-1)):
+                        minesAmount+=1
+                    if(self.hasMine(i+1, j+1)):
+                        minesAmount+=1
+                    if(self.hasMine(i-1, j+1)):
+                        minesAmount+=1
+                self.__board[i][j].setMineAmount(minesAmount)
+
+
 
 class Minesweeper(tk.Frame):
     def __init__(self, master=None):
@@ -164,6 +417,13 @@ class Minesweeper(tk.Frame):
                             endFlag = False
             if endFlag:
                 tk.messagebox.showinfo(" ", "Wygrana")
+
+
+
+
+
+
+
 root = tk.Tk()
-app = Minesweeper(master=root)
-app.mainloop()
+saper = Minesweeper(master=root)
+saper.mainloop()
